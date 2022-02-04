e62593221383664234a6ddc6b6fe640e2540a941
Jamila Tomines <jtomines@uvic.ca>
2022-02-04 11:32:15 -0800

    Formatted input

--- AnalyzeTCP.py
+++ AnalyzeTCP.py
@@ -1,5 +1,21 @@
 import sys
+import re
+import pyshark
 
-# take in the string cap file name
-capfile = sys.argv[1]
-print(capfile)
+cappath = sys.argv[1] # take in the string cap file name
+capname = ''
+# check valid input:
+if re.search(r'/', cappath) and re.search(r'\.cap', cappath):
+    match = re.match(r'(.*)/(.*\.cap)',cappath)
+    capname = match[1]
+    print(match.groups())
+elif re.search(r'\.cap', cappath):
+    capname = cappath
+    print(cappath)
+else:
+    sys.exit('Input invalid: not a .cap file')
+
+# open .cap file
+cap = pyshark.LiveCapture(output_file=capname)
+cap.sniff(timeout=32)
+cap
